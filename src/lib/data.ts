387fd58df6dc38f66464f4901e84441be3ebf8bc
rkumar0101 <rishavkumarkarn7@gmail.com>
2025-06-13 13:30:56 +0530

    Configure production Strapi URL

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,6 +1,6 @@
 // src/lib/data.ts
 
-const STRAPI_URL = 'http://127.0.0.1:1337';
+const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_API_URL || 'http://127.0.0.1:1337';
 
 export type Article = {
   id: string;
